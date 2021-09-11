ecb1dea517722a1a71efe63f0630df27ca025395
Max Glaser <mpf.glaser@gmail.com>
2021-09-11 17:51:19 +0200

    KNOW-3 Added properties to Article.kt

--- backend/src/main/kotlin/nl/mpfglaser/knowl3dge/model/Article.kt
+++ backend/src/main/kotlin/nl/mpfglaser/knowl3dge/model/Article.kt
@@ -1,4 +1,27 @@
 package nl.mpfglaser.knowl3dge.model
 
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "articles")
 class Article {
+    @Id
+    @Column(name = "id")
+    var id: Int = -1
+
+    @Column(name = "author_id")
+    var author_id: Int = -1
+
+    @Column(name = "creation_date")
+    var creation_date: Date = Date()
+
+    @Column(name = "edit_date")
+    var edit_date: Date = Date()
+
+    @Column(name = "content")
+    var content: String = "No content found"
 }
