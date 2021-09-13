4ae281b1c1a9f7450c8dace7f9fbf500c1e6bd8d
Max Glaser <mpf.glaser@gmail.com>
2021-09-13 19:45:31 +0200

    Re-added hibernate dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.hibernate:hibernate-core:5.5.7.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
 	runtimeOnly("mysql:mysql-connector-java:8.0.25")
 }
