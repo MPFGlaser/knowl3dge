ecf10e8a169353990a0f4de49a5bf683df127fd5
Max Glaser <mpf.glaser@gmail.com>
2021-10-07 15:05:16 +0200

    Added form to article-editor and made it auto-populate

--- frontend/knowl3dge-webapp/src/app/app.module.ts
+++ frontend/knowl3dge-webapp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import { ArticleDetailsComponent } from './article-details/article-details.component';
 import { ArticleEditorComponent } from './article-editor/article-editor.component';
 
@@ -21,15 +24,21 @@ import { ArticleEditorComponent } from './article-editor/article-editor.componen
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatCardModule,
     MatDividerModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ArticleListComponent },
       { path: 'articles/:articleId', component: ArticleDetailsComponent },
+      { path: 'articles/edit', component: ArticleEditorComponent },
+      { path: 'articles/edit/:articleId', component: ArticleEditorComponent },
     ]),
   ],
   declarations: [
--- frontend/knowl3dge-webapp/src/app/article-editor/article-editor.component.ts
+++ frontend/knowl3dge-webapp/src/app/article-editor/article-editor.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from '../interfaces/article';
+import { ArticleService } from './../services/article.service';
 
 @Component({
   selector: 'app-article-editor',
   templateUrl: './article-editor.component.html',
-  styleUrls: ['./article-editor.component.scss']
+  styleUrls: ['./article-editor.component.scss'],
 })
 export class ArticleEditorComponent implements OnInit {
+  article: Article | undefined;
 
-  constructor() { }
+  editForm = this.formBuilder.group({
+    Title: '',
+    Author: '',
+    Content: '',
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
+
+  onSubmit(): void {
+    console.warn('Article edited', this.editForm.value);
+    this.editForm.reset();
   }
 
+  ngOnInit(): void {
+
+    const routeParams = this.route.snapshot.paramMap;
+    console.warn(routeParams);
+
+    const articleIdFromRoute = Number(routeParams.get('articleId'));
+    console.warn(routeParams);
+    this.getArticle(articleIdFromRoute);
+  }
+
+  async getArticle(id: number) {
+    const article = await this.articleService.getArticle(id).toPromise();
+    this.article = article;
+  }
 }
