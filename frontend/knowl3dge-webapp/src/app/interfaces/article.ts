c24e172e587f5b57c69093f2dbc8b8138e72040c
Max Glaser <mpf.glaser@gmail.com>
2021-10-07 17:19:17 +0200

    Removed fake data from Article

--- frontend/knowl3dge-webapp/src/app/interfaces/article.ts
+++ frontend/knowl3dge-webapp/src/app/interfaces/article.ts
@@ -3,37 +3,7 @@ export interface Article {
   authorId: number;
   title: string;
   content: string;
-  creationDate: string;
-  editDate: string;
+  creationDate: number;
+  editDate: number;
   visible: boolean;
 }
-
-export const articles = [
-  {
-    id: 1,
-    authorId: 1,
-    title: 'title here',
-    content: 'article content lorem ipsum dolor sit amet',
-    creationDate: '0',
-    editDate: '0',
-    visible: true,
-  },
-  {
-    id: 2,
-    authorId: 1,
-    title: 'title2 here',
-    content: 'article content lorem ipsum dolor sit amet',
-    creationDate: '0',
-    editDate: '0',
-    visible: true,
-  },
-  {
-    id: 3,
-    authorId: 2,
-    title: 'title3 here',
-    content: 'article content lorem ipsum dolor sit amet',
-    creationDate: '0',
-    editDate: '0',
-    visible: true,
-  },
-];
